b1b0e4fb792a65ec0437736d8edcab9cf53d4112
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-06-28 10:22:33 +0300

    Create basic Express structure

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class AuthController {
+    async registration(req, res) {
+        try {
+
+        } catch (err) {
+
+        }
+    }
+
+    async login(req, res) {
+        try {
+
+        } catch (err) {
+            
+        }
+    }
+
+    async getUsers(req, res) {
+        try {
+
+        } catch (err) {
+            
+        }
+    }
+}
+
+module.exports = new AuthController();
